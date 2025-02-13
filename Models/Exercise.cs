fd411bdf98b56ea545adb3807d0826ae26b45285
alicecorry <alicecorry@icloud.com>
2025-02-13 19:12:59 +0000

    Scaffolding Exercise (and editing some styling in the Create)

---
+++ Models/Exercise.cs
@@ -0,0 +1,10 @@
+namespace PhysioMvc.Models;
+
+public class Exercise
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? VideoUrl { get; set; }
+    public string? ImageUrl { get; set; }
+}
